7e8207e67abc0be6a79cac7b2897e601a3a4766c
brandoyts <brando.endona.dev@gmail.com>
2022-04-28 18:34:05 +0800

    initial commit

---
+++ components/Container.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type ContainerProps = {
+    children: JSX.Element[] | JSX.Element
+}
+const Container = ({ children }: ContainerProps) => {
+    return (
+        <div className="container p-5 max-w-7xl m-auto">
+            {children}
+        </div>
+    )
+}
+
+export default Container
